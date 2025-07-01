f33481bdded7e6568506a0de84d82d4b87c5c56d
Natan Almeida <natanalmeida158@hotmail.com>
2025-07-01 08:19:23 -0300

    update POO

---
+++ 00-POO/01-pilares_poo.py
@@ -0,0 +1,48 @@
+
+# Exemplo de herança
+print("\nExemplo de herança:")
+class Animal:
+    def __init__(self, nome) -> None:
+        self.nome = nome
+
+    def andar(self):
+        print(f"O animal {self.nome} andou")
+        return  
+    
+    def emitir_som(self):
+        pass
+
+class Cachorro(Animal):
+    def emitir_som(self):
+        return "Au, Au"
+    
+class Gato(Animal):
+    def emitir_som(self):
+        return "Miau!"
+    
+
+dog = Cachorro(nome="Rex")
+cat = Gato(nome="Felix")
+
+print("\nExemplo de polimorfismo")
+animais = [dog, cat]
+
+for animal in animais:
+    print(f"{animal.nome} faz: {animal.emitir_som()}")
+
+print("\nExemplo de encapsulamento:")
+class ContaBancaria:
+    def __init__(self, saldo) -> None:
+        self.__saldo = saldo # Atributo privado
+    
+    def depositar(self, valor):
+        if valor > 0:
+            self.__saldo += valor
+    
+    def sacar(self, valor):
+        if valor > 0 and valor <= self.__saldo:
+            self.__saldo -= valor
+
+    def consultar_saldo(self):
+        return self.__saldo
+
---
+++ readme.md
@@ -0,0 +1,14 @@
+# 🐍 Meus Estudos em Python
+
+---
+
+Este repositório é dedicado aos meus estudos e práticas em Python. Aqui você encontrará uma variedade de projetos, exercícios e implementações que desenvolvo para aprimorar minhas habilidades e explorar diferentes conceitos da linguagem.
+
+## 🎯 Objetivo
+
+O principal objetivo deste repositório é:
+
+* **Consolidar o aprendizado:** Ter um local centralizado para armazenar e revisar os tópicos e projetos que estou estudando.
+* **Acompanhar o progresso:** Observar minha evolução e desenvolvimento na programação Python ao longo do tempo.
+* **Referência rápida:** Servir como um recurso para consultar implementações de algoritmos, estruturas de dados ou soluções para problemas específicos.
+* **Compartilhar conhecimento (opcional):** Se você estiver aprendendo Python, sinta-se à vontade para explorar e usar este repositório como referência.
